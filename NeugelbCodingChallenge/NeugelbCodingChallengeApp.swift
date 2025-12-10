8939318ddb565beb7798ef750a6fda9697bfa767
Abdul Haseeb <ahaseeb91@gmail.com>
2025-12-10 23:48:47 +0100

    FirstCommit

---
+++ NeugelbCodingChallenge/NeugelbCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeugelbCodingChallengeApp.swift
+//  NeugelbCodingChallenge
+//
+//  Created by Abdul Haseeb on 10.12.25.
+//
+
+import SwiftUI
+
+@main
+struct NeugelbCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeugelbCodingChallengeTests/NeugelbCodingChallengeTests.swift
@@ -0,0 +1,16 @@
+//
+//  NeugelbCodingChallengeTests.swift
+//  NeugelbCodingChallengeTests
+//
+//  Created by Abdul Haseeb on 10.12.25.
+//
+
+import Testing
+
+struct NeugelbCodingChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
